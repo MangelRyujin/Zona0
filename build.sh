819a1ddb88a68d84d89fc79199e93d3ddb275a70
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2023-11-23 12:37:32 -0500

    modified build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-poetry install -r ./requirements/production.txt
+poetry install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
