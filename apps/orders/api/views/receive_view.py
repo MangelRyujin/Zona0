301df338bf59cf4984d7542bb5101b32e76fec15
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2024-01-09 14:12:48 -0500

    add swagger_auto_schema

--- apps/orders/api/views/receive_view.py
+++ apps/orders/api/views/receive_view.py
@@ -2,15 +2,53 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
 from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
 from apps.orders.api.serializers.transfer_serializer import ReceiveOSPSerializer
 from rest_framework import status
 from apps.users.models import User
 from apps.orders.models import ReceiveOSP
-from utils.send_email.send_email import send_email_transfer
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 
+# 'method' can be used to customize a single HTTP method of a view
+receive_response = openapi.Response('response description', ReceiveOSPSerializer)
+list_unpaid_receive={
+  "id": 0,
+  "user": 0,
+  "amount": "string",
+  "code": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "state": "Unpaid",
+  "image": "string",
+  "date": "2024-01-09",
+  "time": "string"
+}
+list_paid_receive={
+  "id": 0,
+  "user": 0,
+  "amount": "string",
+  "code": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "state": "Paid",
+  "image": "string",
+  "date": "2024-01-09",
+  "time": "string"
+}
+
+
+@swagger_auto_schema(method='get', manual_parameters=None, responses={200: f'{list_paid_receive}'})
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def ListPaidReceiveOSPView(request):
+    user = get_object_or_404(User, pk=request.user.id)
+    receiver = ReceiveOSP.objects.filter(user=user).filter(state='Paid').order_by('-date', '-time')[:30]
+    if receiver:
+        transfer_serializer = ReceiveOSPSerializer(receiver, many=True)
+        return Response(transfer_serializer.data, status = status.HTTP_200_OK)
+    return Response({'message':'No existen recibos pagados'}, status=status.HTTP_404_NOT_FOUND)
+
+
+test_param = openapi.Parameter('amount', openapi.IN_QUERY, description="enter an amount", type=openapi.TYPE_NUMBER)
+@swagger_auto_schema(method='post', manual_parameters=[test_param], responses={201: receive_response})
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def CreateReceiveOSPView(request):
@@ -25,16 +63,7 @@ def CreateReceiveOSPView(request):
     return Response({'message':'Usuario no existe'}, status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def ListPaidReceiveOSPView(request):
-    user = get_object_or_404(User, pk=request.user.id)
-    receiver = ReceiveOSP.objects.filter(user=user).filter(state='Paid').order_by('-date', '-time')[:30]
-    if receiver:
-        transfer_serializer = ReceiveOSPSerializer(receiver, many=True)
-        return Response(transfer_serializer.data, status = status.HTTP_200_OK)
-    return Response({'message':'No existen recibos pagados'}, status=status.HTTP_404_NOT_FOUND)
-  
+@swagger_auto_schema(method='get', manual_parameters=None, responses={200: f'{list_unpaid_receive}'})
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def ListUnpaidReceiveOSPView(request):
@@ -47,6 +76,8 @@ def ListUnpaidReceiveOSPView(request):
         return Response({'message':'No existen solicitudes de recibos'}, status=status.HTTP_404_NOT_FOUND)
     return Response({'message':'Usuario no existe'}, status=status.HTTP_404_NOT_FOUND)
 
+
+@swagger_auto_schema(method='delete', responses={200: 'Solicitud eliminada'})
 @api_view(["DELETE"])
 @permission_classes([IsAuthenticated])
 def DeleteUnpaidReceiveOSPView(request,id):
